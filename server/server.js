db40153636579233f0b91ea5cff6eadb4799eb37
Tou Xiong <touyeexiong@gmail.com>
2023-03-10 17:45:43 -0600

    set up nodemon along with npm start script for the server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,12 @@ require("dotenv").config();
 
 const app = express();
 
+app.get("/getRestaurants", (req, res) => {
+    console.log('get all restaurants');
+    
+})
+//http://localhost:
+
 const port = process.env.PORT || 3005;
 app.listen(port, () => {
   console.log(`server is up and listening on port ${port}`);
