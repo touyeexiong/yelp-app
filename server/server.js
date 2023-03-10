6ac66dbcc35f5596fd6c430cc3314b413075e713
Tou Xiong <touyeexiong@gmail.com>
2023-03-10 17:40:21 -0600

    setup ports through dotenv and setup backup port.

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
 const express = require("express");
-
-
-
-
+require("dotenv").config();
 
 const app = express();
 
-app.listen(3005, () =>{
-    console.log('server is up and listening on port 3005');
-    
+const port = process.env.PORT || 3005;
+app.listen(port, () => {
+  console.log(`server is up and listening on port ${port}`);
 });
+
+// don't want to hardcode as 3005, cause you may change ports in development and production environment. going to install dotenv
