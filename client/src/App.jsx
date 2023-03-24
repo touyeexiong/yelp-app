9db3bc208a2eab786a97c4fa1845dc0e8ef40dc3
Tou Xiong <touyeexiong@gmail.com>
2023-03-23 20:17:35 -0500

    installed react-router-dom into the wrong directory. Reverted back to last commit and installed into correct one. Routers are working. Also, tutorial video is using react-router-dom v5.4, we've refactored for v6.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./routes/Home";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import UpdatePage from "./routes/UpdatePage";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+        <Route path="/" element={<UpdatePage />} />
+
+      </Routes>
+    </Router>
+
+  </div>;
 };
 
 export default App;
