738f7430f545983c99470b7371c7e03c7e266a23
Tou Xiong <touyeexiong@gmail.com>
2023-04-02 22:25:19 -0500

    now mapping and rendering list onto dom.

--- client/src/components/RestaurantList.jsx
+++ client/src/components/RestaurantList.jsx
@@ -9,25 +9,24 @@ import { RestaurantContext } from "../context/RestaurantsContext";
 //             const response = await RestaurantFinder.get("/");
 //             console.log(response.data.data.restaurants);
 //           } catch (error) {}
-          
+
 //     }
 //     useEffect(() => {
 //         getRestaurantList();
 //     }, []);
-    // the array allows it to only run when it mounts, preventing an infinite loop
+// the array allows it to only run when it mounts, preventing an infinite loop
 const RestaurantList = (props) => {
-    const { restaurants, setRestaurants } = useContext(RestaurantContext);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await RestaurantFinder.get("/");
-                setRestaurants(response.data.data.restaurants);
-            } catch (error) {}
-        };
-
-        fetchData();
-    }, [])
-
+  const { restaurants, setRestaurants } = useContext(RestaurantContext);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await RestaurantFinder.get("/");
+        setRestaurants(response.data.data.restaurants);
+      } catch (error) {}
+    };
+    console.log(restaurants);
+    fetchData();
+  }, []);
 
   return (
     <div className="list-group">
@@ -43,7 +42,26 @@ const RestaurantList = (props) => {
           </tr>
         </thead>
         <tbody>
-          <tr className="bg-secondary">
+          <tr>{/* {JSON.stringify({restaurants})} */}</tr>
+          {/* && placed there to say if restaurants exist, run code and if not don't
+                prevents it from showing restaurants that don't exists.
+            */}
+          {restaurants &&
+            restaurants.map((restaurant) => {
+              return (
+                <tr>
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.location}</td>
+                  <td>{"$".repeat(restaurant.price_range)}</td>
+                  <td>reviews</td>
+                  <td>
+                    <button className="btn btn-warning">Update</button>
+                  </td>
+                  <button className="btn btn-danger">Delete</button>
+                </tr>
+              );
+            })}
+          {/* <tr className="bg-secondary">
             <td>mcdonalds</td>
             <td>New York</td>
             <td>$$</td>
@@ -66,7 +84,7 @@ const RestaurantList = (props) => {
             <td>
               <button className="btn btn-danger">Delete</button>
             </td>
-          </tr>
+          </tr> */}
         </tbody>
       </table>
     </div>
