01ffd45e408cd08d71747aab9e827eb22749a0ab
Tou Xiong <touyeexiong@gmail.com>
2023-04-10 21:23:45 -0500

    set up form and am retrieving data based off params with a useEffect hook.

--- client/src/components/UpdateRestaurant.jsx
+++ client/src/components/UpdateRestaurant.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import RestaurantFinder from "../apis/RestaurantFinder";
 
 const UpdateRestaurant = (props) => {
   const { id } = useParams();
   const [name, setName] = useState("");
-  const [locaiton, setLocation] = useState("");
+  const [location, setLocation] = useState("");
   const [priceRange, setPriceRange] = useState("");
+
+  useEffect(() => {
+    const fetchData = async() => {
+        const response = await RestaurantFinder.get(`/${id}`)
+        console.log(response.data.data);
+        setName(response.data.data.restaurant.name);
+        setLocation(response.data.data.restaurant.location);
+        setPriceRange(response.data.data.restaurant.price_range);
+    };
+
+    fetchData();
+  }, [])
 
   console.log(id);
   return (
@@ -15,6 +28,7 @@ const UpdateRestaurant = (props) => {
           <label htmlFor="name">Name</label>
           <input
             id="name"
+            value={name}
             onChange={(e) => setName(e.target.value)}
             className="form-control"
             type="text"
@@ -24,6 +38,7 @@ const UpdateRestaurant = (props) => {
           <label htmlFor="location">Location</label>
           <input
             id="location"
+            value={location}
             onChange={(e) => setLocation(e.target.value)}
             className="form-control"
             type="text"
@@ -33,6 +48,7 @@ const UpdateRestaurant = (props) => {
           <label htmlFor="price_range">Price Range</label>
           <input
             id="price_range"
+            value={priceRange}
             onChange={(e) => setPriceRange(e.target.value)}
             className="form-control"
             type="number"
