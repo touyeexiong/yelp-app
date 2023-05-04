7cf2d368bfd8878212358d2cede5b22f069ed9a1
Tou Xiong <touyeexiong@gmail.com>
2023-05-03 21:50:41 -0500

    filtered for free stars and found a half one for free. I'm dumb.

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -5,8 +5,8 @@ const StarRating = ({ rating }) => {
   for (let i = 1; i <= 5; i++) {
     if (i <= rating) {
       stars.push(<i className="fa-solid fa-star"></i>);
-    // } else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
-    //   stars.push(<i className="fa-solid fa-star-sharp-half-stroke"></i>);
+    } else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
+      stars.push(<i className="fa-solid fa-star-half-stroke"></i>);
     } else {
       stars.push(<i className="fa-regular fa-star"></i>);
     }
