5ebff57f3c6ccc367f4ad29bf74d121861db969e
Tou Xiong <touyeexiong@gmail.com>
2023-07-21 23:56:22 -0500

    post addReview route now working on Postman. changed variable from results to newReview

--- server/server.js
+++ server/server.js
@@ -119,13 +119,13 @@ app.delete("/api/v1/restaurants/:id", async (req, res) => {
 
 app.post("/api/v1/restaurants/:id/addReview", async (req, res) => {
   try {
-    console.log('we in addReview');
+    console.log('we in addReview', req.params.id, req.body.name, req.body.review, req.body.rating);
     
-    const results = await db.query(
+    const newReview = await db.query(
       "INSERT INTO reviews (restaurant_id, name, review, rating) values ($1, $2, $3, $4) returning *",
       [req.params.id, req.body.name, req.body.review, req.body.rating]
     );
-    console.log(newReview);
+    console.log(newReview, "is it working?");
     
     res.status(201).json({
       status: "success",
