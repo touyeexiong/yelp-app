9073908fac9bde29dc36af69ff1683a6d0e49afd
Tou Xiong <touyeexiong@gmail.com>
2023-12-21 21:29:49 -0600

    clearning up components now

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -17,7 +17,6 @@ const AddRestaurant = () => {
             price_range: priceRange
         })
         addRestaurants(response.data.data.restaurant);
-        console.log(response);
     } catch (error) {
         console.log(error);
     }
